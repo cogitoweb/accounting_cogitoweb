3e1a23898c0a80566bf3f6f5a946f368b092b2ec
2z <paolo.cazzitti@cogitoweb.it>
2017-03-13 11:25:10 +0100

    refact

--- move_mutual_cogitoweb/model/account_mutual.py
+++ move_mutual_cogitoweb/model/account_mutual.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api, exceptions
+from openerp import models, fields, api, exceptions, _
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -65,82 +65,55 @@ class cogito_account_mutual(models.Model):
 
 
 
-	@api.one
-	def generate_cogito_mutual(self, default={}, context=None, *args, **kwargs):
+	@api.multi
+	def generate_cogito_mutual(self):
 		# Duplica un record di account_mode lanciando super().copy() 
 		# e tutte le account_move_line collegate.
 		# Lancia una query per trovare le account_move_line collegate
 		# e invertine le colonne date/avere
 		# Impedisce di creare duplicati di duplicati
 
-		if not default:
-			default = {}
+        tot_records = len(self)
+        processed_records = 0
+        messages = ""
+        for s in self:
 
-		if self.ref == False:
-			# Se ref non e' definito, non posso cercare
-			# o duplicare. Ferma tutto!
-			raise exceptions.ValidationError("Il campo 'riferimento' non e' valido.")
+            if s.ref == False:
+                # Se ref non e' definito, non posso cercare
+                messages += ("\nIl campo 'riferimento' non e' valido per il record %s" % s.id)
 
-		# Se esiste gia un duplicato, ferma tutto
-		reciproco_id = self.cerca_reciproco(self.id, self.ref)
-		if reciproco_id != False:
-			raise exceptions.ValidationError("Il reciproco esiste gia'")
+            # Se esiste gia un duplicato, ferma tutto
+            reciproco_id = s.cerca_reciproco(s.id, s.ref)
+            if reciproco_id != False:
+                messages += ("\nIl reciproco il record %s esiste già" % s.id)
 
+            duplicato_id = super(cogito_account_mutual, s).copy()
+            duplicato_obj = self.env['account.move'].browse(duplicato_id)
 
-		# copy(cr, uid, id, default=None, context=None)
-		# Duplicate record with given id updating it with default values
+            # ### Query update nome/riferimento
+            duplicato_obj.write({
+                'name':"Storno " + s.ref,
+                'ref':"Storno " + s.ref
+            })
 
-		# cr -- database cursor
-		# uid -- current user id
-		# id -- id of the record to copy
-		# default (dictionary) -- dictionary of field values to override in the original values of the copied record, e.g: {'field_name': overriden_value, ...}
-		# context (dictionary) -- context arguments, like lang, time zone
-		duplicato = super(cogito_account_mutual, self).copy(default, context=context)
+            for row in duplicato_obj.line_id:
 
+                debit_new = row.credit
+                credit_new = row.debit
 
-		# ### ID del nuovo record
-		reciproco_id = int(duplicato)
-		_logger.info('<><><> EHI, duplicato con ID %d', (reciproco_id))
+                row.write(
+                    {
+                        'debit': debit_new,
+                        'credit': credit_new
+                    }
+                )
 
+            processed_records += 1
 
-		# ### Query update nome/riferimento
-		ref_new = "Storno " + self.ref
-		name_new = "Storno " + self.ref
-		query_update = """
-				UPDATE account_move
-				SET name = '%s', ref = '%s'
-				WHERE id = %d
-			""" % (name_new, ref_new, reciproco_id)
+        return {
+         'type': 'ir.actions.act_window.message',
+         'title': _('Message'),
+         'message': _("Created %s multual movements of %s moves selected" % (tot_records, processed_records)) + "\n\n" + messages,
+         'close_button_title': _('Close'),
+         }
 
-		self.env.cr.execute(query_update)
-
-
-
-		# ### Query cerca account_move_line
-		query_search = """
-				SELECT id, debit, credit, move_id 
-				FROM account_move_line WHERE move_id = %d
-			""" % (reciproco_id)
-
-		self.env.cr.execute(query_search)
-		query_result = self.env.cr.dictfetchall()
-		num_result = len(query_result)
-		
-		for row in range(0, num_result):
-			line_id = query_result[row]['id']
-			debit_new = query_result[row]['credit']
-			credit_new = query_result[row]['debit']
-
-			query_update = """
-					UPDATE account_move_line
-					SET debit = %s, credit = %s 
-					WHERE id = %s
-				""" % (debit_new, credit_new, line_id)
-
-			# ### Query di scambio colonne
-			self.env.cr.execute(query_update)
-
-
-
-		_logger.info('<><><> EHI, generate_cogito_mutual ha terminato')
-		return duplicato
